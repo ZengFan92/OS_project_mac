dd44facb8899c080ec1c3093ec0f016f5e69e880
ZengFan92 <wuditharll@gmail.com>
2017-04-26 14:38:42 -0700

    change readtime method, change function attr
    Use assembly read time macro. 
    Use no inline attribute to avoid compiler put function inline

--- Test_CPU.cpp
+++ Test_CPU.cpp
@@ -5,15 +5,29 @@
 #include <unistd.h>
 
 #include <sys/types.h>
-#include <pthread.h>
+//#include <pthread.h>
 #include <sys/syscall.h>
 
 #define ITERATION 100
+#define Cyc2Time 1
 static __inline__ uint64_t rdtsc(void) {
     unsigned hi, lo;
     __asm__ __volatile__ ("rdtsc" : "=a"(lo), "=d"(hi));
     return ( (uint64_t)lo)|( ((uint64_t)hi)<<32 );
 }
+
+// use assembly MACRO to read RDTSC to avoid procedure call overhead
+#define read_start_time(hi,lo) \
+asm volatile ("CPUID\n\t" \
+"RDTSC\n\t" \
+"mov %% edx, %0\n\t" \
+"mov %% eax, %1\n\t": "=r" (hi), "=r" (lo):: "%rax","%rbx", "%rcx", "%rdx") 
+
+#define read_end_time(hi,lo) \
+asm volatile ("RDTSC\n\t" \
+"mov %% edx, %0\n\t" \
+"mov %% eax, %1\n\t" \
+"CPUID\n\t": "=r" (hi), "=r" (lo):: "%rax","%rbx", "%rcx", "%rdx")
 
 //1: Measurement overhead
 //1-1: Overhead of reading time
@@ -23,12 +37,15 @@ static double ReadTimeOverhead() {
 	//long double sum = 0;
     double sum = 0;
 	for (int i = 0; i < 1000; ++i) {
-		start = rdtsc();
-		end = rdtsc();
-		sum += end - start;
+        unsigned hi, lo,hi1, lo1;
+        read_start_time(hi,lo);
+        read_end_time(hi1,lo1);
+        start = ( (uint64_t)lo)|( ((uint64_t)hi)<<32 );
+        end = ( (uint64_t)lo1)|( ((uint64_t)hi1)<<32 );
+        sum += end - start;
 	}
 	sum /= 1000.0;
-	return sum / 2.9;
+	return sum / Cyc2Time;
 }
 
 //1-2: Overhead of using a loop to measure many iterations of an operation
@@ -39,82 +56,148 @@ static double LoopOverhead() {
 	//long double sum = 0;
     double sum = 0;
 	for (int i = 0; i < 1000; i++) {
-		start = rdtsc();
+        unsigned hi, lo,hi1, lo1;
+        read_start_time(hi,lo);
 		for (int j = 0; j < 10000; j++) {
 			//Empty loop, the only operation is inc(j)
 		}
-		end = rdtsc();
+        read_end_time(hi1,lo1);
+        start = ( (uint64_t)lo)|( ((uint64_t)hi)<<32 );
+        end = ( (uint64_t)lo1)|( ((uint64_t)hi1)<<32 );
 		sum += (end - start) / 10000.0;
 	}
 	sum /= 1000.0;
-	return sum / 2.9;
+	return sum / Cyc2Time;
 }
 
 //2: Procedure call overhead
-static void TestArgument_0() {}
-static void TestArgument_1(int a) {}
-static void TestArgument_2(int a, int b) {}
-static void TestArgument_3(int a, int b, int c) {}
-static void TestArgument_4(int a, int b, int c, int d) {}
-static void TestArgument_5(int a, int b, int c, int d, int e) {}
-static void TestArgument_6(int a, int b, int c, int d, int e, int f) {}
-static void TestArgument_7(int a, int b, int c, int d, int e, int f, int g) {}
+// use noinline attribute to prevent compiler making the function inline
+static __attribute__((noinline)) void TestArgument_0 () {}
+static __attribute__((noinline)) void TestArgument_1(int a) {}
+static __attribute__((noinline)) void TestArgument_2(int a, int b) {}
+static __attribute__((noinline)) void TestArgument_3(int a, int b, int c) {}
+static __attribute__((noinline)) void TestArgument_4(int a, int b, int c, int d) {}
+static __attribute__((noinline)) void TestArgument_5(int a, int b, int c, int d, int e) {}
+static __attribute__((noinline)) void TestArgument_6(int a, int b, int c, int d, int e, int f) {}
+static __attribute__((noinline)) void TestArgument_7(int a, int b, int c, int d, int e, int f, int g) {}
 static double ProcedureOverhead(int num) {
 	uint64_t start = 0;
 	uint64_t end = 0;
 	//uint64_t elapsed;
 	//long double sum = 0;
     double sum = 0;
-	for (int i = 0; i < 1000; i++) {
-		switch (num) {
-			case 0:
-				start = rdtsc();
+    switch (num) {
+        case 0:
+            for (int i = 0; i < 1000; i++) {
+                unsigned hi, lo,hi1, lo1;
+                read_start_time(hi,lo);
+                // function here
                 TestArgument_0();
-				end = rdtsc();
-				break;
-			case 1:
-				start = rdtsc();
+                // function ends
+                read_end_time(hi1,lo1);
+                start = ( (uint64_t)lo)|( ((uint64_t)hi)<<32 );
+                end = ( (uint64_t)lo1)|( ((uint64_t)hi1)<<32 );
+                sum += (end - start);
+            }
+            break;
+        case 1:
+            for (int i = 0; i < 1000; i++) {
+                unsigned hi, lo,hi1, lo1;
+                read_start_time(hi,lo);
+                // function here
                 TestArgument_1(1);
-				end = rdtsc();
-				break;
-			case 2:
-				start = rdtsc();
+                // function ends
+                read_end_time(hi1,lo1);
+                start = ( (uint64_t)lo)|( ((uint64_t)hi)<<32 );
+                end = ( (uint64_t)lo1)|( ((uint64_t)hi1)<<32 );
+                sum += (end - start);
+            }
+            break;
+        case 2:
+            for (int i = 0; i < 1000; i++) {
+                unsigned hi, lo,hi1, lo1;
+                read_start_time(hi,lo);
+                // function here
                 TestArgument_2(1,1);
-				end = rdtsc();
-				break;
-			case 3:
-				start = rdtsc();
+                // function ends
+                read_end_time(hi1,lo1);
+                start = ( (uint64_t)lo)|( ((uint64_t)hi)<<32 );
+                end = ( (uint64_t)lo1)|( ((uint64_t)hi1)<<32 );
+                sum += (end - start);
+            }
+            break;
+        case 3:
+            for (int i = 0; i < 1000; i++) {
+                unsigned hi, lo,hi1, lo1;
+                read_start_time(hi,lo);
+                // function here
                 TestArgument_3(1,1,1);
-				end = rdtsc();
-				break;
-			case 4:
-				start = rdtsc();
+                // function ends
+                read_end_time(hi1,lo1);
+                start = ( (uint64_t)lo)|( ((uint64_t)hi)<<32 );
+                end = ( (uint64_t)lo1)|( ((uint64_t)hi1)<<32 );
+                sum += (end - start);
+            }
+            break;
+        case 4:
+            for (int i = 0; i < 1000; i++) {
+                unsigned hi, lo,hi1, lo1;
+                read_start_time(hi,lo);
+                // function here
                 TestArgument_4(1,1,1,1);
-				end = rdtsc();
-				break;
-			case 5:
-				start = rdtsc();
+                // function ends
+                read_end_time(hi1,lo1);
+                start = ( (uint64_t)lo)|( ((uint64_t)hi)<<32 );
+                end = ( (uint64_t)lo1)|( ((uint64_t)hi1)<<32 );
+                sum += (end - start);
+            }
+            break;
+        case 5:
+            for (int i = 0; i < 1000; i++) {
+                unsigned hi, lo,hi1, lo1;
+                read_start_time(hi,lo);
+                // function here
                 TestArgument_5(1,1,1,1,1);
-				end = rdtsc();
-				break;
-			case 6:
-				start = rdtsc();
+                // function ends
+                read_end_time(hi1,lo1);
+                start = ( (uint64_t)lo)|( ((uint64_t)hi)<<32 );
+                end = ( (uint64_t)lo1)|( ((uint64_t)hi1)<<32 );
+                sum += (end - start);
+            }
+            break;
+        case 6:
+            for (int i = 0; i < 1000; i++) {
+                unsigned hi, lo,hi1, lo1;
+                read_start_time(hi,lo);
+                // function here
                 TestArgument_6(1,1,1,1,1,1);
-				end = rdtsc();
-				break;
-			case 7:
-				start = rdtsc();
+                // function ends
+                read_end_time(hi1,lo1);
+                start = ( (uint64_t)lo)|( ((uint64_t)hi)<<32 );
+                end = ( (uint64_t)lo1)|( ((uint64_t)hi1)<<32 );
+                sum += (end - start);
+            }
+            break;
+        case 7:
+            for (int i = 0; i < 1000; i++) {
+                unsigned hi, lo,hi1, lo1;
+                read_start_time(hi,lo);
+                // function here
                 TestArgument_7(1,1,1,1,1,1,1);
-				end = rdtsc();
-				break;
-            default:
-                start = rdtsc();
-                end = rdtsc();
-		}
-		sum += (end - start);
-	}
+                // function ends
+                read_end_time(hi1,lo1);
+                start = ( (uint64_t)lo)|( ((uint64_t)hi)<<32 );
+                end = ( (uint64_t)lo1)|( ((uint64_t)hi1)<<32 );
+                sum += (end - start);
+            }
+            break;
+        default:
+            break;
+    }
+	
+    
 	sum /= 1000.0;
-	return sum / 2.9;
+	return sum / Cyc2Time;
 }
 
 //3: System call overhead
@@ -130,7 +213,7 @@ static double SystemOverhead() {
 		sum += end - start;
 	}
 	sum /= 1000.0;
-	return sum/2.9;
+	return sum/Cyc2Time;
 }
 
 //4: Task creation time
@@ -150,7 +233,7 @@ static double TaskCreationTime() {
 		else{		//parent
 			//wait(NULL);
 			//end = rdtsc();
-			total_time += (end - start)/2.9;
+			total_time += (end - start)/Cyc2Time;
 		}
 	}
 	return total_time / ITERATION;
@@ -172,7 +255,7 @@ uint64_t OneSwitchTime(int *fd) {
         // read(fd[0], (void*)&end, sizeof(end));
 	}
     if(end > start){
-        elapsed = (end - start)/2.9;
+        elapsed = (end - start)/Cyc2Time;
     }
     return elapsed;
 }
@@ -208,7 +291,7 @@ static double PipeOverhead() {
 		write(fd[1], &start, sizeof(start));
 		read(fd[0], &start, sizeof(start));
 		end = rdtsc();
-		elapsed = (end - start)/2.9;
+		elapsed = (end - start)/Cyc2Time;
 		total_time += elapsed;
 	}
 	return (double)total_time / ITERATION;
@@ -216,6 +299,7 @@ static double PipeOverhead() {
 
 
 //Create kernel thread overhead
+/*
 void *Task() {
 	pthread_exit(0);
 }
@@ -228,10 +312,11 @@ static double KernelOverhead() {
 	pthread_create(&thread, NULL, Task, NULL);
 	end = rdtsc();
 	elapsed = (end - start);
-	return elapsed / 2.9;
+	return elapsed / Cyc2Time;
 }
 
 //Context switch kernel thread overhead
+
 static uint64_t thread_end;
 static uint64_t thread_start;
 static uint64_t cs_time;
@@ -252,9 +337,9 @@ static double CSKernelOverhead() {
 	pthread_create(&t1, NULL, &Thread1, NULL);
 	pthread_join(t1, NULL);
 	cs_time = thread_end - thread_start;
-	return cs_time / 2.9;
+	return cs_time / Cyc2Time;
 }
-
+*/
 int main(int argc, const char * argv[]) {
 	double overhead;
 
@@ -285,11 +370,12 @@ int main(int argc, const char * argv[]) {
 	printf("Process context switch overhead = %lf nanoseconds\n", overhead);
 
 	//kernel thread
-	overhead = KernelOverhead();
-	printf("kernel thread overhead = %lf nanoseconds\n", overhead);
+	//overhead = KernelOverhead();
+	//printf("kernel thread overhead = %lf nanoseconds\n", overhead);
 
 	//kernel thread context switch time
-	overhead = CSKernelOverhead();
-	printf("kernel thread context switch overhead = %lf nanoseconds\n", overhead);
-	return 0;
+	//overhead = CSKernelOverhead();
+	//printf("kernel thread context switch overhead = %lf nanoseconds\n", overhead);
+    //exit(0);
+	//return 0;
 }
